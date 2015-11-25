914085feb599780fa6cffe0ab260d388da8d7f56
Denine Guy <denine_guy@cable.comcast.com>
2015-11-24 23:46:33 -0500

    added methods to check for correct answer and update users score

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var allQuestions = [
   {
-    question: "Who is the Prime Minisiter of England in 2015?",
-    choices: ["Margaret Thatcher", "David Cameron", "Tony Blair", "John Major"],
-    correctAnswer:1
+    question: 'Who is the Prime Minister of England in 2015?',
+    choices: ['Margaret Thatcher', 'David Cameron', 'Tony Blair', 'John Major'],
+    correctAnswer: 1
   }
 ];
 
@@ -17,7 +17,7 @@ question.textContent = selectQuestion(questionNumber);
 function selectQuestion(number) {
   if (allQuestions.length > 0) {
     return allQuestions[number].question;
-  }  
+  }
 }
 
 function answerOptions(number) {
@@ -25,18 +25,18 @@ function answerOptions(number) {
   var div;
   var element;
   var radioLabel;
-  for (var i=0; i < choices.length; i++) {
+  for (var i = 0; i < choices.length; i++) {
     div = document.createElement('div');
     element = document.createElement('input');
-    element.setAttribute('id', choices[i]);
+    element.setAttribute('id', 'option');
     element.setAttribute('type', 'radio');
     element.setAttribute('name', 'name');
     element.setAttribute('value', choices[i]);
-    
+
     radioLabel = document.createElement('label');
     radioLabel.setAttribute('for', choices[i]);
     radioLabel.textContent = choices[i];
-  
+
     insertAfter(question, div);
     div.appendChild(element);
     div.appendChild(radioLabel);
@@ -44,17 +44,36 @@ function answerOptions(number) {
 }
 
 function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 function checkAnswer(e, number) {
   e.preventDefault();
-  console.log(number);
+  var form = document.getElementById('questionForm');
+  // var answer = form.elements.option.value;``
+  var selectedAnswer = form.elements.option;
+  for (var i = 0; i < selectedAnswer.length; i++) {
+    if (selectedAnswer[i].checked === true) {
+      selectedAnswer = i;
+    }
+  }
+  // may need to move this code
+  if (selectedAnswer === allQuestions[0].correctAnswer) {
+    number++;
+    questionNumber = number;
+  }
+
+  showScore(questionNumber);
 }
 
 elForm.addEventListener('submit', function(e) {
   checkAnswer(e, questionNumber);
 }, false);
 
+function showScore(number) {
+  document.getElementById('score').textContent = number;
+}
 
-
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   console.log("DOM fully loaded and parsed");
+// });
